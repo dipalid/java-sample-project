2d9a090c570a9ccf5599bceebe18b31600472c62
dipalid <dipali.dongare@tcs.com>
2016-02-11 18:20:47 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,9 +1,5 @@
 public class abc { 
    public static void main(String[] args) { 
-<<<<<<< HEAD
       System.out.println(" ****Welcome Hello, World***");
-=======
-      System.out.println("Welcome Hello, World");
->>>>>>> cb0f197a30ba07846726f8ab2d2b59c8a0a29768
    }
 }
