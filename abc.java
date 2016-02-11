e6cc6e28807b73555d64427300b4e7d984d0ce8e
dipali <dipali.dongare@tcs.com>
2016-02-11 18:23:11 +0530

    tcs committed

--- abc.java
+++ abc.java
@@ -1,9 +1,5 @@
 public class abc { 
-   public static void main(String[] args) { 
-<<<<<<< HEAD
-      System.out.println(" ****Welcome Hello, World***");
-=======
-      System.out.println("Welcome Hello, World");
->>>>>>> cb0f197a30ba07846726f8ab2d2b59c8a0a29768
-   }
+   public static void main(String[] args) {
+      System.out.println("-----Welcome To TCS----");
+  }
 }
