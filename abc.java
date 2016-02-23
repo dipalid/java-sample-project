25324f3c3a963be96906cdfc21a15a1ea1a1a51f
dipalid <dipali.dongare@tcs.com>
2016-02-23 10:37:37 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,5 +1,5 @@
 public class abc { 
    public static void main(String[] args) { 
-      System.out.println(" ****Welcome to tcs***");
+      System.out.println(" *******Hello World*******");
    }
 }
